0bdc658cdb7e85647782ac1146f225e22814a3cd
ayuayue <1401262639@qq.com>
2021-02-09 11:11:07 +0800

    edit config file and add autostart patch

--- config.mk
+++ config.mk
@@ -4,11 +4,15 @@ VERSION = 6.2
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX = /usr
 MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
+
+# X11INC = /usr/X11R6/X11
+# X11LIB = /usr/include/X11
+
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
